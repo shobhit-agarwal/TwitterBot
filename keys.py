3b92f21f2c2b536a65712ff55e0fe8881af2cee4
Vipul Vaibhaw <vaibhaw.vipul@gmail.com>
2016-10-25 01:38:43 +0530

    Create keys.py

---
+++ keys.py
@@ -0,0 +1,37 @@
+__author__ = 'vipulvaibhaw'
+
+class WeatherBot:
+    def con_secret(self):
+        a = ""
+        return a
+    def con_secret_key(self):
+        b = ""
+        return b
+    def token(self):
+        c = ""
+        return c
+    def token_key(self):
+        d = ""
+        return d
+
+class RandomQuoteTweeterBot:
+    def con_secret(self):
+        a = ""
+        return a
+    def con_secret_key(self):
+        b = ""
+        return b
+    def token(self):
+        c = ""
+        return c
+    def token_key(self):
+        d = ""
+        return d
+
+class GoodReads:
+    def key(self):
+        a = ""
+        return a
+    def secret(self):
+        b = ""
+        return b
